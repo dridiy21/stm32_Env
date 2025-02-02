35e05b2651375fc0d8c02f11e0c9530e525740a8
Yassine Dridi <yassine.dridi@expleogroup.com>
2025-02-02 17:58:30 +0100

    test

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,28 +1,23 @@
 pipeline {
     agent any
-    environment {
-        DOCKER_IMAGE = "yassined97/my_new_stmdevenv:latest"
-        WORKSPACE_PATH = "/workspace"
-    }
     stages {
-        stage('Initialization') {
+        stage('Check User') {
             steps {
-                echo "New push detected"
+                sh 'whoami'
             }
         }
         stage('Build in Docker') {
             steps {
                 script {
                     sh """
-                    docker run -i --rm \
-                        -v \$(pwd):${WORKSPACE_PATH} ${DOCKER_IMAGE} /bin/bash -c '
-                        cd ${WORKSPACE_PATH}/stm32_Env &&
+                    sudo -u jenkins docker run -it --rm \\
+                        -v \$(pwd):/workspace yassined97/my_new_stmdevenv:latest /bin/bash -c '
+                        cd stm32_Env/workspace &&
                         git pull origin main &&
-                        cd ${WORKSPACE_PATH}/stm32_Env/workspace &&
-                        /opt/st/stm32cubeide_1.15.0/stm32cubeide --launcher.suppressErrors -nosplash \
-                            -application org.eclipse.cdt.managedbuilder.core.headlessbuild \
-                            -data "./" \
-                            -import "./UART_Transmit/" \
+                        /opt/st/stm32cubeide_1.15.0/stm32cubeide --launcher.suppressErrors -nosplash \\
+                            -application org.eclipse.cdt.managedbuilder.core.headlessbuild \\
+                            -data "./" \\
+                            -import "./UART_Transmit/" \\
                             -build UART_Transmit
                     '
                     """
@@ -30,36 +25,4 @@ pipeline {
             }
         }
     }
-    post {
-        always {
-            echo "Pipeline completed."
-        }
-        failure {
-            echo "Build failed!"
-        }
-    }
 }
-
-
-
-
-
-
-
-
-pipeline {
-    agent any
-    
-    stages {
-        stage('Hello') {
-            steps {
-                checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/dridiy21/stm32_Env.git']])
-                echo 'Hello, World!'
-            }
-        }
-    }
-}
-
-
-
-//random comment
