6d467f41208fd90b1247dc51b0c6cfb1cf3297a9
Yassine Dridi <yassine.dridi@expleogroup.com>
2025-02-02 18:52:03 +0100

    test7

--- pipeline.groovy
+++ pipeline.groovy
@@ -12,18 +12,12 @@ pipeline {
                     sh """
                     docker run -i --rm \\
                         -v \$(pwd):/workspace yassined97/my_new_stmdevenv:latest /bin/bash -c '
-                        whoami &&
-                        pwd &&
-                        ls &&
-                        cd workspace &&
-                        ls
-                        #stm32_Env/workspace &&
-                        #git pull origin main &&
-                        #/opt/st/stm32cubeide_1.15.0/stm32cubeide --launcher.suppressErrors -nosplash \\
-                        #    -application org.eclipse.cdt.managedbuilder.core.headlessbuild \\
-                        #    -data "./" \\
-                        #    -import "./UART_Transmit/" \\
-                        #    -build UART_Transmit
+                        git pull origin main &&
+                        /opt/st/stm32cubeide_1.15.0/stm32cubeide --launcher.suppressErrors -nosplash \\
+                            -application org.eclipse.cdt.managedbuilder.core.headlessbuild \\
+                            -data "./" \\
+                            -import "./UART_Transmit/" \\
+                            -build UART_Transmit
                     '
                     """
                 }
