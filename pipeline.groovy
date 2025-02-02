33adb32b8aca6cc070f58e54dd30d13d56a88516
Yassine Dridi <yassine.dridi@expleogroup.com>
2025-02-02 19:12:51 +0100

    test999

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,22 +1,31 @@
 pipeline {
     agent any
+    environment {
+        DOCKER_IMAGE = "yassined97/my_new_stmdevenv:latest"
+        WORKSPACE_PATH = "/workspace"
+    }
     stages {
-        stage('Check User') {
+        stage('Initialization') {
             steps {
-                sh 'whoami'
+                echo "New push detected"
             }
         }
         stage('Build in Docker') {
             steps {
                 script {
                     sh """
-                    docker run -i --rm \\
-                        -v \$(pwd):/workspace yassined97/my_new_stmdevenv:latest /bin/bash -c '
+                    docker run -i --rm \
+                        -v \$(pwd):${WORKSPACE_PATH} ${DOCKER_IMAGE} /bin/bash -c '
+                        #cd ${WORKSPACE_PATH}/stm32_Env &&
+                        ls &&
+                        cd workspace &&
+                        ls &&
                         git pull origin main &&
-                        /opt/st/stm32cubeide_1.15.0/stm32cubeide --launcher.suppressErrors -nosplash \\
-                            -application org.eclipse.cdt.managedbuilder.core.headlessbuild \\
-                            -data "./" \\
-                            -import "./UART_Transmit/" \\
+                        #cd ${WORKSPACE_PATH}/stm32_Env/workspace &&
+                        /opt/st/stm32cubeide_1.15.0/stm32cubeide --launcher.suppressErrors -nosplash \
+                            -application org.eclipse.cdt.managedbuilder.core.headlessbuild \
+                            -data "./" \
+                            -import "./UART_Transmit/" \
                             -build UART_Transmit
                     '
                     """
@@ -24,4 +33,12 @@ pipeline {
             }
         }
     }
-}   
+    post {
+        always {
+            echo "Pipeline completed."
+        }
+        failure {
+            echo "Build failed!"
+        }
+    }
+}
