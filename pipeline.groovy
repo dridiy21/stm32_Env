033eb762a48beaa7ec3063b84d21c4a2b05d85ec
Yassine Dridi <yassine.dridi@expleogroup.com>
2025-02-02 18:08:13 +0100

    test2

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,7 +10,7 @@ pipeline {
             steps {
                 script {
                     sh """
-                    sudo -u jenkins docker run -it --rm \\
+                    docker run -it --rm \\
                         -v \$(pwd):/workspace yassined97/my_new_stmdevenv:latest /bin/bash -c '
                         cd stm32_Env/workspace &&
                         git pull origin main &&
