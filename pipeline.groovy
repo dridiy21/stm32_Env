91116468f6d621c6dfbb1cbeea137eacf3cff5e6
dridiy21 <68065889+dridiy21@users.noreply.github.com>
2025-09-20 21:41:46 +0200

    refactoring pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,7 +2,14 @@ pipeline {
     agent any
 
     triggers {
-        githubPush()  // Detect GitHub webhooks (push events)
+        githubPush()  // Déclenche le pipeline sur push vers GitHub
+    }
+
+    environment {
+        STM32CUBEIDE = "/opt/st/stm32cubeide_1.15.0/stm32cubeide"
+        WORKSPACE_DIR = "workspace"
+        PROJECT_NAME = "UART_Transmit"
+        REPO_URL = "https://github.com/dridiy21/stm32_Env.git"
     }
 
     stages {
@@ -11,18 +18,24 @@ pipeline {
                 checkout scmGit(
                     branches: [[name: '*/main']],
                     extensions: [],
-                    userRemoteConfigs: [[url: 'https://github.com/dridiy21/stm32_Env.git']]
+                    userRemoteConfigs: [[url: "${REPO_URL}"]]
                 )
             }
         }
 
-        stage('Run Docker') {
+        stage('Build inside Docker') {
             steps {
-                sh '''
+                sh """
                     docker run --rm \
-                    yassined97/my_new_stmdevenv:latest \
-                    bash -c "whoami && cd stm32_Env && git pull && cd workspace && /opt/st/stm32cubeide_1.15.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data ./ -import ./UART_Transmit/ -build UART_Transmit"
-                '''
+                    -v \$(pwd):/workspace \\
+                    yassined97/my_new_stmdevenv:latest \\
+                    bash -c "
+                        whoami && \
+                        cd /workspace/stm32_Env/${WORKSPACE_DIR} && \
+                        git pull origin main && \
+                        ${STM32CUBEIDE} --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data ./ -import ./${PROJECT_NAME}/ -build ${PROJECT_NAME}
+                    "
+                """
             }
         }
     }
