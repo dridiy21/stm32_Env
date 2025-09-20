f5c1db85f09c158303bac40761cbc3284cf0224c
dridiy21 <68065889+dridiy21@users.noreply.github.com>
2025-09-20 23:01:35 +0200

    Update pipeline.groovy refact

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,6 +4,15 @@ pipeline {
     triggers {
         githubPush()  // Detect GitHub webhooks (push events)
     }
+    
+    environment {
+        DOCKER_IMG = "yassined97/my_new_stmdevenv:latest"
+        GIT_REPO = "/workspace/stm32_Env"
+        STM32CUBEIDE = "/opt/st/stm32cubeide_1.15.0/stm32cubeide"
+        WORKSPACE_DIR = "workspace"
+        PROJECT_NAME = "UART_Transmit"
+        REPO_URL = "https://github.com/dridiy21/stm32_Env.git"
+    }
 
     stages {
         stage('Checkout') {
@@ -19,9 +28,14 @@ pipeline {
         stage('Run Docker') {
             steps {
                 sh '''
-                    docker run --rm \
-                    yassined97/my_new_stmdevenv:latest \
-                    bash -c "whoami && cd stm32_Env && git pull && cd workspace && /opt/st/stm32cubeide_1.15.0/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data ./ -import ./UART_Transmit/ -build UART_Transmit"
+                    docker run --rm ${DOCKER_IMG} \
+                    bash -c "
+                    whoami && \
+                    cd ${GIT_REPO} && \
+                    git pull && \
+                    cd ${WORKSPACE_DIR} && \
+                    ${STM32CUBEIDE} --launcher.suppressErrors -nosplash -application \
+                    org.eclipse.cdt.managedbuilder.core.headlessbuild -data ./ -import ./UART_Transmit/ -build ${PROJECT_NAME}"
                 '''
             }
         }
